e5b01efa8ff5bdcbc754e2599e489e507133dd2c
edu <aeonszx2@gmail.com>
2026-07-08 15:48:20 -0300

    chore: update url cors to test ambient

--- app/main.py
+++ app/main.py
@@ -36,7 +36,7 @@ origins = [
     "http://localhost:5174",
     "http://localhost:8080",
     "https://stars.soujunior.tech",
-    "https://stars-webapp-three.vercel.app",
+    "stars-webapp-eight.vercel.app",
 ]
 
 app.add_middleware(
